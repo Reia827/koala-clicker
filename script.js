dc3e7c02ebdfd1cb961f05cd1437d92230f6d3d8
Rachel Rawlings <rachel@Reias-MacBook-Air.local>
2018-07-12 19:56:33 -0700

    js practice

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
-// 1. get the element we want (maybe document.getElementByID?)
+// 1. get the element we want, attach listener
 
-// 2. elVariableName.click
-//    or addEventListener syntax (see debugging project for example)
+let clickable = document.querySelector("img.clickable")
+
+clickable.addEventListener("click", clickCounter)
 
 // declare the counter variable
 
+let counter = 0
+console.log("counter: ", counter)
+
 //click handler function
 
-// click handler
+function clickCounter(){
+
 
 // track total clicks--increment the counter variable
+    
+    // counter = counter + 1
 
-//output click count to container in DOM
+counter++
+    
+    //output click count to container in DOM
+    
+    console.log("counter", counter)
+    
+    // get the container, update its contents
+    
+    let container = document.querySelector("p#clickcount")
+    container.innerHTML = counter
+}
+    
+    
+
+/* var button = document.getElementById("clickme"),
+  count = 0;
+button.onclick = function() {
+  count += 1;
+  button.innerHTML = "Click me: " + count;
+}; */
+
+

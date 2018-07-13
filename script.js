083b6ffb07b129f4906f09d695817a7515aa0e66
Rachel Rawlings <rachel@Reias-MacBook-Air.local>
2018-07-12 19:26:11 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// 1. get the element we want (maybe document.getElementByID?)
+
+// 2. elVariableName.click
+//    or addEventListener syntax (see debugging project for example)
+
+// declare the counter variable
+
+//click handler function
+
+// click handler
+
+// track total clicks--increment the counter variable
+
+//output click count to container in DOM
